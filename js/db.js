adb2e7bd53cd6a287ae786b4ad9974819c1742bf
cjosigwe432 <cofl83329@gmail.com>
2026-04-01 12:36:11 +0100

    Add Firebase config and initialize Firestore
    Added Firebase configuration and initialized Firestore.

--- js/db.js
+++ js/db.js
@@ -1,1 +1,15 @@
+// 🔥 PASTE YOUR FIREBASE CONFIG HERE
+const firebaseConfig = {
+  apiKey:  "AIzaSyDLvK-Q4v1hZ2B-mtxZzNXs5m1tCS6LdGw",
+  authDomain: "-3ebbe.firebaseapp.com",
+  projectId: "-app-3ebbe",
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// LOADER
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  if (loader) loader.style.display = "none";
+});
