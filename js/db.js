615a24a0124a2ead85884a1ef2910837131a47b7
cjosigwe432 <cofl83329@gmail.com>
2026-04-01 13:19:14 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,17 +1,10 @@
-// 🔥 PASTE YOUR FIREBASE CONFIG HERE
 const firebaseConfig = {
-apikey: "AIzaSyDLvK-Q4v1hZ2B-mtxZzNXs5m1tCS6LdGw",
+  apiKey: "AIzaSyDLvK-Q4v1hZ2B-mtxZzNXs5m1tCS6LdGw",
   authDomain: "neobank-app-3ebbe.firebaseapp.com",
   projectId: "neobank-app-3ebbe",
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
-// REMOVE LOADER AFTER PAGE LOAD
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  if (loader) {
-    loader.style.display = "none";
-  }
-});
+const auth = firebase.auth();
+const db = firebase.firestore();
