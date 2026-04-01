9f9f1f0a8281d7a5f96f5a4e4da900a065f34757
cjosigwe432 <cofl83329@gmail.com>
2026-04-01 12:38:12 +0100

    Implement user authentication and message loading

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
+// CHECK LOGIN
+firebase.auth().onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("userEmail").innerText = user.email;
+    loadMessages();
+  }
+});
 
+// LOAD MESSAGES
+function loadMessages() {
+  db.collection("messages").get().then(snapshot => {
+    let html = "";
+
+    snapshot.forEach(doc => {
+      const data = doc.data();
+
+      html += `
+        <div>
+          <h4>${data.name}</h4>
+          <p>${data.email}</p>
+          <p>${data.message}</p>
+          <hr>
+        </div>
+      `;
+    });
+
+    document.getElementById("messages").innerHTML = html;
+  });
+}
+
+// LOGOUT
+function logout() {
+  firebase.auth().signOut().then(() => {
+    window.location.href = "index.html";
+  });
+}
